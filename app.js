120dc37f8c8bb9e7119d9ca9edcd9e50f35d1a9b
Mehin Melikova <mhmelikova@gmail.com>
2022-06-21 16:46:10 +0400

    https://github.com/Melikova/breakout_game.git

--- app.js
+++ app.js
@@ -120,12 +120,16 @@ function checkForCollisions(){
         changeDirection();
     }
 
+    // user colision
+
     if(
         (ballCurrentPosition[0]>currentPosition[0] && ballCurrentPosition[0]<(currentPosition[0]+blockWidth))&&
         (ballCurrentPosition[1]>currentPosition[1] && ballCurrentPosition[1]<(currentPosition[1]+blockHeight))   
     ){
         changeDirection();
     }
+
+    // game over
 
     if(ballCurrentPosition[1]<=0){
         clearInterval(timerId);
